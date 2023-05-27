c43eaf969e696f477df08c63f60102da46d6240a
Robbie <robbiethewadley@gmail.com>
2023-05-26 23:12:50 -0600

    update tests & messages

--- test/mdn/checkPartialSupport.js
+++ test/mdn/checkPartialSupport.js
@@ -11,7 +11,7 @@ test('partial support flags unsupported key: value pairs', async (t) => {
     browsers: ['chrome 81'],
     onFeatureUsage: (usageinfo) => {
       const messageWithoutFile = usageinfo.message.replace(/<.*>/, '');
-      t.equal(messageWithoutFile, ':1:8: CSS Appearance appearance: auto is not supported by chrome 81 (css-appearance)');
+      t.equal(messageWithoutFile, ':1:8: CSS Appearance value of auto is not supported by: Chrome (81) (css-appearance)');
       featureCount += 1;
     },
   })).process(css);
@@ -58,7 +58,7 @@ test('partial support messages with many versions are formatted correctly', asyn
     browsers: ['chrome > 70', 'firefox > 60'],
     onFeatureUsage: (usageinfo) => {
       const messageWithoutFile = usageinfo.message.replace(/<.*>/, '');
-      t.equal(messageWithoutFile, ':1:8: CSS Appearance appearance: auto is not supported by Chrome (81,80,79,78,77,76,75,74,73,72,71), Firefox (79,78,77,76,75,74,73,72,71,70,69,68,67,66,65,64,63,62,61) (css-appearance)');
+      t.equal(messageWithoutFile, ':1:8: CSS Appearance value of auto is not supported by: Chrome (81,80,79,78,77,76,75,74,73,72,71), Firefox (79,78,77,76,75,74,73,72,71,70,69,68,67,66,65,64,63,62,61) (css-appearance)');
       featureCount += 1;
     },
   })).process(css);

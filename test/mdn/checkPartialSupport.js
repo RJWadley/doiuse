d1cc19ee0a8c4fdfcab183ebbee0bd6149496662
Robbie <robbiethewadley@gmail.com>
2023-05-26 22:33:42 -0600

    format multiple versions properly

--- test/mdn/checkPartialSupport.js
+++ test/mdn/checkPartialSupport.js
@@ -49,3 +49,20 @@ test('partial support does not flag formerly unsupported key: value pairs', asyn
   // make sure onFeatureUsage was called
   t.equal(featureCount, 0);
 });
+
+test('partial support messages with many versions are formatted correctly', async (t) => {
+  const css = '.test {appearance: auto;}';
+  let featureCount = 0;
+
+  await postcss(new DoIUse({
+    browsers: ['chrome > 70', 'firefox > 60'],
+    onFeatureUsage: (usageinfo) => {
+      const messageWithoutFile = usageinfo.message.replace(/<.*>/, '');
+      t.equal(messageWithoutFile, ':1:8: CSS Appearance appearance: auto is not supported by Chrome (81,80,79,78,77,76,75,74,73,72,71), Firefox (79,78,77,76,75,74,73,72,71,70,69,68,67,66,65,64,63,62,61) (css-appearance)');
+      featureCount += 1;
+    },
+  })).process(css);
+
+  // make sure onFeatureUsage was called
+  t.equal(featureCount, 1);
+});
